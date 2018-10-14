f6d5cdf90559b323e2fb5a877ec670f7f71df863
Lou Huang <lou@louhuang.com>
2018-10-14 12:08:35 -0400

    download to z/x/y tile format

--- node-downloader.js
+++ node-downloader.js
@@ -1,21 +1,44 @@
-const download = require('image-downloader')
+const fs = require('fs')
+const path = require('path')
+const axios = require('axios')
+const mkdirp = require('mkdirp')
+
 const base_URL = 'http://web.mta.info/weekender/images/subwaytilesnew/'
 const size = 16
+const MAX_X_TILE = 26
+const MAX_Y_TILE = 26
+// const size = 15
+// const MAX_X_TILE = 13
+// const MAX_Y_TILE = 13
 
-for (let x = -1; x <= 26; x++){
-  for (let y = -1; y <= 26; y++) {
+async function downloadImage ({ size, x, y } = options) {
+  const url = `${base_URL}${size}_${x}_${y}.png`
+  const response = await axios.request({
+    url,
+    method: 'get',
+    responseType: 'stream'
+  }).catch((err) => {
+    console.log('Unable to get', url)
+  })
 
-    const options = {
-      url: `${base_URL}${size}_${x}_${y}.png`,
-      dest: './images'                  // Save to /path/to/dest/image.jpg
+  const destDir = path.join(__dirname, `./images/${size}/${(9650 * 2) + x}/`)
+
+  mkdirp(destDir, function (err) {
+    if (err) {
+      console.error(err)
+    } else {
+      const filename = `${destDir}${(12300 * 2) + y}.png`
+      if (response && response.data && response.data.pipe) {
+        response.data.pipe(fs.createWriteStream(filename))
+        console.log(`Writing ${filename}`)
+      }
     }
-    download.image(options)
-      .then(({ filename, image }) => {
-        console.log('File saved to', filename)
-      }).catch((err) => {
-        console.log('couldnt save', options.url)
-        throw err
-      })
+  })
+}
+
+for (let x = 0; x <= MAX_X_TILE; x++){
+  for (let y = 0; y <= MAX_Y_TILE; y++) {
+    downloadImage({ size, x, y })
   }
 }
 
