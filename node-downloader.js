364e0b768800b9c98e40469ce4b0e805a78137c8
Lou Huang <lou@louhuang.com>
2018-10-14 14:09:16 -0400

    z14?

--- node-downloader.js
+++ node-downloader.js
@@ -4,12 +4,15 @@ const axios = require('axios')
 const mkdirp = require('mkdirp')
 
 const base_URL = 'http://web.mta.info/weekender/images/subwaytilesnew/'
-const size = 16
-const MAX_X_TILE = 26
-const MAX_Y_TILE = 26
+// const size = 16
+// const MAX_X_TILE = 26
+// const MAX_Y_TILE = 26
 // const size = 15
 // const MAX_X_TILE = 13
 // const MAX_Y_TILE = 13
+const size = 14
+const MAX_X_TILE = 9
+const MAX_Y_TILE = 9
 
 async function downloadImage ({ size, x, y } = options) {
   const url = `${base_URL}${size}_${x}_${y}.png`
@@ -21,13 +24,13 @@ async function downloadImage ({ size, x, y } = options) {
     console.log('Unable to get', url)
   })
 
-  const destDir = path.join(__dirname, `./images/${size}/${(9650 * 2) + x}/`)
+  const destDir = path.join(__dirname, `./images/${size}/${(9650 / 2) + x}/`)
 
   mkdirp(destDir, function (err) {
     if (err) {
       console.error(err)
     } else {
-      const filename = `${destDir}${(12300 * 2) + y}.png`
+      const filename = `${destDir}${(12300 / 2) + y}.png`
       if (response && response.data && response.data.pipe) {
         response.data.pipe(fs.createWriteStream(filename))
         console.log(`Writing ${filename}`)
